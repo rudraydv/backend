4fa6e0fadd690b7dcbe1fc9e14a3e36c67c34f0a
rudrayadav <rudracom429@gmail.com>
2024-12-09 18:17:54 +0530

    okk

--- backend/router/paymentRouter.js
+++ backend/router/paymentRouter.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {processPayment, sendStripeApiKey} = require('../controllers/paymentController');
-const {isAuthenticatedUser} = require('../middlerware/authcookie');
 
 
-
-router.route('/process/payment').post(isAuthenticatedUser, processPayment);
-
-
+router.route('/process/payment').post(processPayment);
 router.route('/stripeapikey').get(sendStripeApiKey);
 
 
